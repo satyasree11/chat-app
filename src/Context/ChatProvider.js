b6d79744867599ee4ddb6a54665fc8cf180fe1fa
dss180803@gmail.com <dss180803@gmail.com>
2023-01-05 06:29:45 +0530

    first  commit

---
+++ src/Context/ChatProvider.js
@@ -0,0 +1,43 @@
+import React, { createContext, useContext, useEffect, useState } from "react";
+
+import { useHistory } from "react-router-dom";
+const ChatContext = createContext();
+const ChatProvider = ({ children }) => {
+const [user, setUser] = useState();
+const history = useHistory();
+const [selectedChat, setSelectedChat] = useState();
+const [chats, setChats] = useState([]);
+ const [notification, setNotification] = useState([]);
+
+
+
+useEffect(() => {
+    const userInfo = JSON.parse(localStorage.getItem("userInfo"));
+    setUser(userInfo);
+
+    if (!userInfo) history.push("/");
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+}, [history]);
+
+  return (
+    <ChatContext.Provider 
+    value={{
+       user,
+       setUser,
+       selectedChat,
+        setSelectedChat,
+        notification,
+        setNotification,
+        chats, 
+        setChats,}}>
+        {children}
+    </ChatContext.Provider>
+  );
+};
+
+export const ChatState = () => {
+  return useContext(ChatContext);
+};
+
+export default ChatProvider;
+
---
+++ src/components/miscellaneous/ProfileModal.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import { useDisclosure } from '@chakra-ui/react';
+import { Button,IconButton} from "@chakra-ui/react";
+import { ViewIcon } from '@chakra-ui/icons';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  Image,Text,
+  ModalCloseButton,
+} from '@chakra-ui/react';
+const ProfileModal =({ user,children}) => {
+ const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+    {children ? (
+        <span onClick={onOpen}>{children}</span>
+      ) : (
+        <IconButton d={{ base: "flex" }} icon={<ViewIcon />} onClick={onOpen} />
+      )}
+      <Modal size="lg" isOpen={isOpen} onClose={onClose} isCentered>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader
+          fontSize="40px"
+            fontFamily="Work sans"
+            d="flex"
+            justifyContent="center">
+              {user.name}
+            </ModalHeader>
+          <ModalCloseButton />
+          <ModalBody
+          d="flex"
+            flexDir="column"
+            alignItems="center"
+            justifyContent="space-between">
+              <Image
+              borderRadius="full"
+              boxSize="150px"
+              src={user.pic}
+              alt={user.name}
+            />
+            <Text
+              fontSize={{ base: "28px", md: "30px" }}
+              fontFamily="Work sans"
+            >
+              Email: {user.email}
+            </Text>
+           
+          </ModalBody>
+            
+          <ModalFooter>
+            <Button colorScheme='blue' mr={3} onClick={onClose}>
+              Close
+            </Button>
+            <Button variant='ghost'>Secondary Action</Button>
+          </ModalFooter>
+        </ModalContent>
+     </Modal>
+  
+  
+    </>
+      
+      
+  );
+};
+
+export default ProfileModal;
